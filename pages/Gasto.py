abf81ec7926f8a78a3316225850d81ad3d6fc76e
Cristian <128819507+fremencl@users.noreply.github.com>
2024-08-12 16:59:26 -0400

    Update Gasto.py
    Actualiza nombre file S3

--- pages/Gasto.py
+++ pages/Gasto.py
@@ -28,7 +28,7 @@ st.markdown(
 # Definimos las URLs de los archivos de referencia
 DATA0_URL = 'https://streamlitmaps.s3.amazonaws.com/Data_0724.csv'
 BUDGET_URL = 'https://streamlitmaps.s3.amazonaws.com/Base_Presupuesto_3.csv'
-ORDERS_URL = 'https://streamlitmaps.s3.amazonaws.com/OT_0724.csv'
+ORDERS_URL = 'https://streamlitmaps.s3.amazonaws.com/Base_Ordenes_0724.csv'
 BASE_UTEC_URL = 'https://streamlitmaps.s3.amazonaws.com/Base_UTEC_BudgetVersion.csv'
 BASE_CECO_URL = 'https://streamlitmaps.s3.amazonaws.com/Base_Ceco_3.csv'
 
