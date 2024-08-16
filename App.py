ebbffd6dc2631fc8c79c37adf91d2fe4f6225202
Cristian <128819507+fremencl@users.noreply.github.com>
2024-08-16 13:57:17 -0400

    Update App.py

--- App.py
+++ App.py
@@ -1,31 +1,43 @@
 import streamlit as st
 
+# Configuración de la aplicación
 st.set_page_config(
-    page_title="Gerencia Gestion de Activos",
+    page_title="Gerencia Gestión de Activos",
     page_icon=":dollar:",
     initial_sidebar_state="expanded",
 )
 
-st.write("### MONITOR DE GESTION PRESUPUESTARIA :chart_with_upwards_trend:")
-st.write("#### GERENCIA GESTION DE ACTIVOS")
+# Insertar el logo de la compañía centrado en la parte superior
+st.markdown(
+    """
+    <div style='text-align: center;'>
+        <img src='assets/logo.jpg' width='150'>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
-st.sidebar.success("Selecciona un modelo de analisis de arriba.")
+# Títulos y subtítulos
+st.write("### MONITOR DE GESTIÓN PRESUPUESTARIA :chart_with_upwards_trend:")
+st.write("#### GERENCIA GESTIÓN DE ACTIVOS")
 
+# Mensaje en la barra lateral
+st.sidebar.success("Selecciona un modelo de análisis de arriba.")
+
+# Contenido introductorio y descripción de la aplicación
 st.markdown(
     """##### Exploraremos tendencias y ratios relacionados con el perfil del Gasto de Mantenimiento de nuestra Gerencia
     
-En la actual etapa de desarrollo de esta aplicacion, estan disponibles 2 modulos: Gastos y Ordenes
+En la actual etapa de desarrollo de esta aplicación, están disponibles 2 módulos: **Gastos** y **Órdenes**.
 
-El Modelo de Gastos te permitirá comparar gasto real y presupuesto bajo diferentes perspectivas.
+- **Modelo de Gastos**: Te permitirá comparar gasto real y presupuesto bajo diferentes perspectivas.
+- **Modelo de Órdenes**: Te permitirá relacionar órdenes y sus respectivos gastos, así como una primera aproximación a KPIs asociados.
 
-El Modelo de Ordenes te permitirá relacionar ordenes y sus respectivos gastos, así como una primera aproximacion a KPI´s asociados.
-
-En ambos modulos cuentas con filtros dinámicos para poder ajustar las vistas de las herramientas de análisis y desagregar la informacion por:
-Año
-Proceso (Producción, Distribución, Recolección o Depuración)
-Tipo de gasto (Materiales o Servicios)
+En ambos módulos cuentas con filtros dinámicos para poder ajustar las vistas de las herramientas de análisis y desagregar la información por:
+- Año
+- Proceso (Producción, Distribución, Recolección o Depuración)
+- Tipo de gasto (Materiales o Servicios)
 
 :moneybag::moneybag::moneybag::moneybag::moneybag::moneybag::moneybag::moneybag::moneybag::moneybag::moneybag::moneybag::moneybag::moneybag::moneybag::moneybag::moneybag:
-
-"""
+    """
 )
